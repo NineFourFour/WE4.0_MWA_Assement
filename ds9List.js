49b07e6c82cc063f0c3c72339f1c4a0c3c538ea4
NineFourFour <james.hickey@webelevate.ie>
2017-05-22 15:22:48 +0100

    Mobile and Web Project

---
+++ ds9List.js
@@ -0,0 +1,110 @@
+{ 
+		"Author":"James Hickey",
+		"Assignment":"WE4.0 Mobile Web Applications, Digital Skills Academy",
+		"Date":"2016/05/16",
+		"Ref":"http://demos.jquerymobile.com/1.2.0/",
+		"Ref":"https://themeroller.jquerymobile.com/",
+		"Ref":"http://learning.digitalskillsacademy.com/",
+		"Ref":"http://www.w3schools.com/json/default.asp",
+		"Ref":"https://en.wikipedia.org/wiki",
+		"Ref":"http://api.jquery.com/jquery.getjson/",
+		"Ref":"http://json.org/",
+		"Ref":"https://forum.jquery.com/topic/dynamically-generated-listview",
+		"description":"information about characters from Star Trek DS9 series",
+
+	"DS9":[
+					{
+						"id":"1",
+						"character":"Benjamin Sisko",
+						"species":"Human",
+						"affiliation":"United Federation of Planets, Starfleet",
+						"position":"Commanding Officer",
+						"rank":"Captain",
+						"src":"images/1.BS.jpg"
+					},
+					{
+						"id":"2",
+						"character":"Worf",
+						"species":"Klingon",
+						"affiliation":"United Federation of Planets, Starfleet, Klingon Empire",
+						"position":"Strategic Operations Officer",
+						"rank":"Lieutenant Commander",
+						"src":"images/2.Worf.jpg"
+					},
+					{
+						"id":"3",
+						"character":"Odo",
+						"species":"Changeling",
+						"affiliation":"Bajoran Militia",
+						"position":"Cheif of Security",
+						"rank":"Constable",
+						"src":"images/3.Odo.jpg"
+					},
+					{
+						"id":"4",
+						"character":"Kira Nerys",
+						"species":"Bajoran",
+						"affiliation":"Bajoran Militia, Starfleet",
+						"position":"First Officer",
+						"rank":"Commander",
+						"src":"images/4.Kira.jpg"
+					},
+					{
+						"id":"5",
+						"character":"Jadzia Dax",
+						"species":"Trill",
+						"affiliation":"United Federation of Planets, Starfleet",
+						"position":"Chief Science Officer",
+						"rank":"Lieutenant Commander",
+						"src":"images/5.Jadzia.jpg"
+					},
+					{
+						"id":"6",
+						"character":"Quark",
+						"species":"Frengi",
+						"affiliation":"Frengi Alliance",
+						"position":"Bar Owner",
+						"rank":"none",
+						"src":"images/6.Quark.jpg"
+					},
+					{
+						"id":"7",
+						"character":"Elim Garak",
+						"species":"Cardassian",
+						"affiliation":"Formerly the Obsidian Order",
+						"position":"Tailor",
+						"rank":"none",
+						"src":"images/7.Garak.jpg"
+					},
+					{
+						"id":"8",
+						"character":"Martok",
+						"species":"Klingon",
+						"affiliation":"Klingon Defence Force",
+						"position":"Chancellor of the Klingon Empire",
+						"rank":"General, Chancellor",
+						"src":"images/8.Martok.jpg"
+					},
+					{
+						"id":"9",
+						"character":"Morn",
+						"species":"Lurian",
+						"affiliation":"none",
+						"position":"none",
+						"rank":"none",
+						"src":"images/9.Morn.jpg"
+				
+					},
+					{
+						"id":"10",
+						"character":"Vic Fontaine",
+						"species":"Hologram",
+						"affiliation":"none",
+						"position":"Singer, Entertainer, Bar Owner",
+						"rank":"none",
+						"src":"images/10.Vic.jpg"
+					}
+					
+					
+	]
+}
